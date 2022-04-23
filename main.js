a62f50f9708e3a6a021f7b435876552ebeb216fc
elmambou <77119578+elmambou@users.noreply.github.com>
2022-04-24 01:12:30 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&display=swap');
+body {
+    font-family: 'Scheherazade New', serif;
+    font-size: 24px;
+    background-color: #FFFF99;
+    max-width: 950px;
+    margin: 50px auto;
+    padding-top: 10px;
+    
+}
+main{ 
+  background-color: white;
+    border: 4px solid black;
+    border-radius:10px;}
+h1 { background-color:#90EE90;
+    margin: 0;
+    text-align: center;
+    border-radius: 5px;
+    border: 5px solid Blue;
+    box-sizing: content-box;
+  
+}
+h2 {
+  padding-left: 20px;
+}
