03ea285c37c5fbfbbd958fed0e20c3db45e56c70
elmambou <77119578+elmambou@users.noreply.github.com>
2022-04-24 01:44:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 const links = [
   { label: 'Week 1', url: 'week01/index.html' }
 ];
+
+const ol = document.getElementById('assignments');   
+links.forEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+  li.appendChild(a);
+  ol.appendChild(li);
+});
+
