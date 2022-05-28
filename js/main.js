61217a9ebf61b3bc49e75052433f2fc8093332df
elmambou <77119578+elmambou@users.noreply.github.com>
2022-05-28 18:19:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const links = [
   { label: 'Week 2', url: 'week02/index.html' },
   { label: 'Week 3', url: 'week03/index.html' },
   { label: 'Week 4', url: 'week04/index.html' },
-  { label: 'Week 5', url: 'week05/index.html' }
+  { label: 'Week 5', url: 'week05/index.html' },
+  { label: 'Week 6', url: 'week06/index.html' }
 ];
 
 const ol = document.getElementById('assignments');   
