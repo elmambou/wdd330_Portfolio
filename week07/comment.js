10ea0bbe116899505c77b7bcbe31e1f2c3ba8cae
elmambou <77119578+elmambou@users.noreply.github.com>
2022-06-04 17:23:37 +0200

    Update comment.js

--- week07/comment.js
+++ week07/comment.js
@@ -63,31 +63,27 @@ class Comments {
         };
     }    
 
-showCommentList(q = null) {
-      try {
-        const parent = document.getElementById(this.commentElementId);
-        if (!parent) throw new Error('comment parent not found');
-        // check to see if the commentUI code has been added yet
-        if (parent.innerHTML === '') {
-          parent.innerHTML = commentUI;
-        }
-        if (q !== null) {
-          // looking at one post, show comments and new comment button
-          document.querySelector('.addComment').style.display = 'block';
-          this.addSubmitListener(q);
-        } else {
-          // no post name provided, hide comment entry
-          document.querySelector('.addComment').style.display = 'none';
-        }
-        // get the comments from the model
-        let comments = this.model.getComments(q);
-        if (comments === null) {
-          // avoid an error if there are no comments yet.
-          comments = [];
-        }
-        renderCommentList(parent.lastChild, comments);
-      } catch (error) {
-        console.log(error);
+    showCommentList(q=null) {
+        try {
+            const parent = document.getElementById('');
+            console.log(parent);
+            if (!parent) {
+                throw new Error('No element found');
+            }
+            if (parent.innerHTML === '') {
+                // parent.innerHTML = commentUI;
+                console.log('rendering');
+            }
+            if (q !== null) {
+                document.querySelector('.addComment').style.display = 'None';
+            }
+            let comments = this.model.getComments(q);
+            if (comments === null) {
+                comments = [];
+            }
+            // renderCommentList(parent.lastChild, comments, postName);
+        } catch (e) {
+            console.log(e);
         }
     }
 }
