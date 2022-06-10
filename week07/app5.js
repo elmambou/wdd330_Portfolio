435d8c39bd25a6a329a4232e3fea135d46a18c2a
elmambou <77119578+elmambou@users.noreply.github.com>
2022-06-10 20:51:56 +0200

    Update app5.js

--- week07/app5.js
+++ week07/app5.js
@@ -2,7 +2,7 @@
 export const hikeList = [
   {
     name: 'Bechler Falls',
-    imgSrc: 'falls.jpg',
+    imgSrc: "https://trailngear.com/wp-content/uploads/2018/11/cascade-falls-32553.jpg",
     imgAlt: 'Image of Bechler Falls',
     distance: '3 miles',
     difficulty: 'Easy',
@@ -13,12 +13,12 @@ export const hikeList = [
    
   },
   {
-    name: 'Teton Canyon',
-    imgSrc: 'falls.jpg',
-    imgAlt: 'Image of Bechler Falls',
+    name: 'Cathedral Rock',
+    imgSrc: "https://trailngear.com/wp-content/uploads/2018/11/Why-the-Climb%E2%80%99s-Worth-the-Time-%E2%80%94-View-from-Cathedral-Rock.jpg",
+    imgAlt: 'Image of Cathedral Rock',
     distance: '3 miles',
     difficulty: 'Easy',
-    description: 'Beautiful short (or long) hike through Teton Canyon.',
+    description: 'Beautiful short (or long) hike through Cathedral Rock',
     directions:
       'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.',
    
@@ -35,13 +35,13 @@ export const hikeList = [
     },
   
   {
-    name: 'Denanda Falls',
-    imgSrc: 'falls.jpg',
-    imgAlt: 'Image of Bechler Falls',
+    name: 'Franklin Falls',
+    imgSrc: "https://trailngear.com/wp-content/uploads/2018/11/Getting-the-Most-Out-of-Washington%E2%80%99s-Finest-Hiking-Spots-Franklin-Falls-1024x678.png",
+    imgAlt: 'Image of Franklin Falls',
     distance: '7 miles',
     difficulty: 'Moderate',
     description:
-      'Beautiful hike through Bechler meadows river to Denanda Falls',
+      'Beautiful hike through Bechler meadows river to Franklin Falls',
     directions:
       'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.',
  
