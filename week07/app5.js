bb407460f7323815d0bf1971b4b6d71f065584a6
elmambou <77119578+elmambou@users.noreply.github.com>
2022-06-10 20:32:22 +0200

    Update app5.js

--- week07/app5.js
+++ week07/app5.js
@@ -1,6 +1,49 @@
-import Hikes from './hikes.js';
-//on load grab the array and insert it into the page
-const myHikes = new Hikes('hikes');
-window.addEventListener('load', () => {
-  myHikes.showHikeList();
-});
+//create an array of hikes
+export const hikeList = [
+  {
+    name: 'Bechler Falls',
+    imgSrc: 'falls.jpg',
+    imgAlt: 'Image of Bechler Falls',
+    distance: '3 miles',
+    difficulty: 'Easy',
+    description:
+      'Beautiful short hike along the Bechler river to Bechler Falls',
+    directions:
+      'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.',
+   
+  },
+  {
+    name: 'Teton Canyon',
+    imgSrc: 'falls.jpg',
+    imgAlt: 'Image of Bechler Falls',
+    distance: '3 miles',
+    difficulty: 'Easy',
+    description: 'Beautiful short (or long) hike through Teton Canyon.',
+    directions:
+      'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.',
+   
+  },
+  
+    {
+        name: "Sturtevant Falls",
+        imgSrc: "https://trailngear.com/wp-content/uploads/2018/11/Sturtevant-Falls-Location-and-Trails.png",
+        imgAlt: "Sturtevant Falls",
+        distance: "3 miles",
+        difficulty: "Relativesly easy",
+        description: "Los Angeles' most popular hiking destination.",
+        directions: "210 freeway, get off on Santa Anita Avenue and head toward the mountains. Santa Anita becames Chantry Flat Rd. Drive to the end and park."
+    },
+  
+  {
+    name: 'Denanda Falls',
+    imgSrc: 'falls.jpg',
+    imgAlt: 'Image of Bechler Falls',
+    distance: '7 miles',
+    difficulty: 'Moderate',
+    description:
+      'Beautiful hike through Bechler meadows river to Denanda Falls',
+    directions:
+      'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.',
+ 
+  }
+];
