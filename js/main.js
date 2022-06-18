0899a00842e90152621a850b76a4433deeaaa1d6
elmambou <77119578+elmambou@users.noreply.github.com>
2022-06-18 15:40:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ const links = {
   ],
 
   block2: [
-      { label: "Week 8 Report and Content", url: 'w08/index.html'},
-      { label: "Week 9 Report and Content", url: 'w09/index.html'},
+      { label: "Week 8 Report and Content", url: "./w08/"},
+      { label: "Week 9 Report and Content", url: "./w09/"},
       { label: "Week 10 Report and Content", url: 'w10/index.html'},
       { label: "Week 11 Report and Content", url: 'w11/index.html'},
       { label: "Week 12 Report and Content", url: 'w12/index.html'},
