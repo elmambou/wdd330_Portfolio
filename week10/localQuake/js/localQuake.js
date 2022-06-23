91b5818b69bf2e5c0a8eae72366d3d628a0746d4
elmambou <77119578+elmambou@users.noreply.github.com>
2022-06-23 23:24:05 +0200

    Update localQuake.js

--- week10/localQuake/js/localQuake.js
+++ week10/localQuake/js/localQuake.js
@@ -1,8 +1,22 @@
-import localQController from './localQController.js';
-import buildNavigation from './routing.js';
-
-const navElement = document.getElementById('mainNav');
-buildNavigation(navElement);
-
-// const mylocalQController = new localQController('#quakeList');
-// mylocalQController.getQuakesByRadius();
+import { getJSON } from './utilities.js';
+// Quake Model
+export default class localQuake {
+  constructor() {
+    this.baseUrl =
+      'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
+    // store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
+    this._quakes = [];
+  }
+  async getEarthQuakesByRadius(position, radius = 100) {
+    this._quakes = await getJSON(
+      this.baseUrl +
+        `&starttime=2019-01-01&endtime=2019-03-02&latitude=${
+          position.lat
+        }&longitude=${position.lon}&maxradiuskm=${radius}`
+    );
+    return this._quakes;
+  }
+  getQuakeById(id) {
+    return this._quakes.features.filter(item => item.id === id)[0];
+  }
+}
