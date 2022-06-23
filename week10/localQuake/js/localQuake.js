90d1eb688167d22223a9e54f140f6944a93b4475
elmambou <77119578+elmambou@users.noreply.github.com>
2022-06-23 21:37:20 +0200

    Delete localQuake.js

--- week10/localQuake/js/localQuake.js
+++
@@ -1,8 +0,0 @@
-import localQController from './localQController.js';
-import buildNavigation from './routing.js';
-
-const navElement = document.getElementById('mainNav');
-buildNavigation(navElement);
-
-// const mylocalQController = new localQController('#quakeList');
-// mylocalQController.getQuakesByRadius();
