590edfd246e94224e3df6fe359d43da7ab5fdc79
elmambou <77119578+elmambou@users.noreply.github.com>
2022-07-02 19:00:05 +0200

    Update main.js

--- week11/client/main.js
+++ week11/client/main.js
@@ -3,11 +3,35 @@ import Auth from './auth.js'
 
 const auth = new Auth();
 
-const button = document.querySelector("#button");
-button.addEventListener("click", auth.login);
+document.getElementById("login").addEventListener("submit", (event) => {
+    event.preventDefault();
 
-makeRequest('login', 'POST', {
-  password: 'user1',
-  email: 'user1@email.com', 
-})
-  ;
+    auth.login(onLoginSuccess);
+});
+
+
+async function onLoginSuccess(token) {
+    document.getElementById("addPost").classList.remove("hidden");
+
+    let data = await makeRequest('posts', 'GET', null, token);
+    document.getElementById("output").innerText = JSON.stringify(data, null, 2);
+}
+
+
+document.getElementById("addPost").addEventListener("submit", async (event) => {
+    let title = document.getElementById("title").value;
+    let content = document.getElementById("content").value;
+    let name = document.getElementById("name").value;
+
+    let body = {
+        title: title,
+        name: name,
+        content: content
+    };
+
+    try {
+        const data = await makeRequest("posts/", "POST", body, auth.token);
+    } catch (error) {
+        console.log(error);
+    }
+});
