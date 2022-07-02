a9f29d3ccd159011bfeaee9ceb4979ee61e37193
elmambou <77119578+elmambou@users.noreply.github.com>
2022-07-03 00:03:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 const links = {
   block1: [
-      { label: "Week 1 Report and Content ", url: "week01/index.html"},
-      { label: "Week 2 Report and Content", url: "week02/index.html"},
-      { label: "Week 3 Report and Content", url: "week03/index.html"},
-      { label: "Week 4 Report and Content", url: "week04/index.html"},
-      { label: "Week 5 Report and Content", url: "week05/index.html"},
-      { label: "Week 6 Report and Content", url: "week06/index.html"},
-      { label: "Week 7 Report and Content", url: "week07/index.html"}
+      { label: "Week 1 Reading and Content ", url: "week01/index.html"},
+      { label: "Week 2 Reading and Content", url: "week02/index.html"},
+      { label: "Week 3 Reading and Content", url: "week03/index.html"},
+      { label: "Week 4 Reading and Content", url: "week04/index.html"},
+      { label: "Week 5 Reading and Content", url: "week05/index.html"},
+      { label: "Week 6 Reading and Content", url: "week06/index.html"},
+      { label: "Week 7 Reading and Content", url: "week07/index.html"}
   ],
 
   block2: [
-      { label: "Week 8 Report and Content", url: "week08/index.html"},
-      { label: "Week 9 Report and Content", url: "week09/index.html"},
-      { label: "Week 10 Report and Content", url: "week10/index.html"},
-      { label: "Week 11 Report and Content", url: "week11/index.html"},
-      { label: "Week 12 Report and Content", url: "week12/teamwork/week11.html"},
+      { label: "Week 8  Reading and Content", url: "week08/index.html"},
+      { label: "Week 9  Reading and Content", url: "week09/index.html"},
+      { label: "Week 10 Reading and Content", url: "week10/index.html"},
     
+       
       { label: " ", url: "#"},
       { label: "  ", url: "#"},
       { label: "Final Project(Under construction)",url: "##"},
